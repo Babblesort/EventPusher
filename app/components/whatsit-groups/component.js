97d7ffb899689137984e7c30ca619abe163497db
Paul McConnell <pmc.consult@gmail.com>
2016-09-16 23:13:55 -0500

    provide a button to add more mock whatsit records

--- app/components/whatsit-groups/component.js
+++ app/components/whatsit-groups/component.js
@@ -2,11 +2,26 @@ import Ember from 'ember';
 import _ from 'lodash/lodash';
 
 export default Ember.Component.extend({
+	store: Ember.inject.service(),
 	items: null,
 	groups: Ember.computed('items.@each.groupKey', function() {
 		return _.groupBy(this.get('items').toArray(), item => item.get('groupKey'));
 	}),
 	groupKeys: Ember.computed('groups.[]', function() {
 		return _.keys(this.get('groups')).sort();
-	})
+	}),
+	nextId: 10,
+	actions: {
+		addOne() {
+			this.get('store').createRecord('whatsit', {
+				id: this.get('nextId'),
+				name: `bar${this.get('nextId')}`,
+				rating: 5,
+				isGood: false,
+				isBad: false,
+				groupKey: 'B'
+			});
+			this.set('nextId', this.get('nextId') + 1);
+		}
+	}
 });
