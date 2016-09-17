25ed700e7079860adf60f5cccd2a37e1dfb09e9e
Paul McConnell <pmc.consult@gmail.com>
2016-09-16 20:41:56 -0500

    create whatsit placeholder model with a few fields.

---
+++ app/whatsit/model.js
@@ -0,0 +1,15 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+
+var { attr } = DS;
+
+export default DS.Model.extend({
+	name: attr('string', { defaultValue: '' }),
+  note: attr('string', { defaultValue: '' }),
+  rating: attr('number'),
+  isGood: attr('boolean', { defaultValue: false }),
+	isBad: attr('boolean', { defaultValue: false }),
+	displayId: Ember.computed('id', function() {
+		return `whatsit:${this.get('id')}`;
+	})
+});
